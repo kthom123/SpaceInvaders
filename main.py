5a7c57c2900a59922cd9bb1de3f23fcbd7a83497
Kyle Thomas <kylejwthomas@gmail.com>
2024-05-29 18:26:04 +0900

    Added game loop to keep window open and close

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ pygame.init()
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
