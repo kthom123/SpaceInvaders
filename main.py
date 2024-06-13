510a2ebcd5b6ce499d8aa3cb0fd22b133ab6cf06
Kyle Thomas <kylejwthomas@gmail.com>
2024-06-13 15:41:01 +0900

    Added background and slowed down enemy

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Background
+background = pygame.image.load('background.jpg')
+
 # Caption and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceship.png')
@@ -40,7 +43,8 @@ while running:
 
     # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
-
+    # Background Image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -67,10 +71,10 @@ while running:
     enemyX += enemyX_change
 
     if enemyX <= 0:
-        enemyX_change = 0.3
+        enemyX_change = 4
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.3
+        enemyX_change = -4
         enemyY += enemyY_change
 
     player(playerX, playerY)
