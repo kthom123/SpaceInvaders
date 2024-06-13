d9d6df71d6e6ad673f9266db1497263085f117f7
Kyle Thomas <kylejwthomas@gmail.com>
2024-06-13 15:58:16 +0900

    Edited background

--- main.py
+++ main.py
@@ -71,10 +71,10 @@ while running:
     enemyX += enemyX_change
 
     if enemyX <= 0:
-        enemyX_change = 4
+        enemyX_change = 0.3
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -4
+        enemyX_change = -0.3
         enemyY += enemyY_change
 
     player(playerX, playerY)
