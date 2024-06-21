a811a37eaad06580351536aa55b385a1b134ad21
Kyle Thomas <kylejwthomas@gmail.com>
2024-06-21 09:57:19 +0900

    Finished game

--- main.py
+++ main.py
@@ -66,15 +66,14 @@ def show_score(x, y):
 
 def game_over_text():
     over_text = over_font.render("GAME OVER", True, (224, 224, 224))
-    screen.blit(over_text, (200, 250))
+    text_rect = over_text.get_rect(center=(800 / 2, 600 / 2))
+    screen.blit(over_text, text_rect.topleft)
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
-
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
-
 
 def fire_bullet(x, y):
     global bullet_state, bulletX, bulletY
@@ -84,14 +83,13 @@ def fire_bullet(x, y):
     screen.blit(bulletImg, (bulletX, bulletY))
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt((math.pow(enemyX-bulletX, 2)) + (math.pow(enemyY-bulletY, 2)))
-    if distance < 27:
-        return True
-    else:
-        return False
+    distance = math.sqrt((math.pow(enemyX - bulletX, 2)) + (math.pow(enemyY - bulletY, 2)))
+    return distance < 27
 
 # Game Loop
 running = True
+game_over = False
+
 while running:
 
     # RGB = Red, Green, Blue
@@ -120,55 +118,57 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-    # Check for boundaries of spaceship
-    playerX += playerX_change
+    if not game_over:
+        # Check for boundaries of spaceship
+        playerX += playerX_change
 
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
-        playerX = 736
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
 
-    # Enemy movement
-    for i in range(num_of_enemies):
+        # Enemy movement
+        for i in range(num_of_enemies):
 
-        # Game Over
-        if enemyY[i] > 440:
-            for j in range(num_of_enemies):
-                enemyY[j] = 2000
-            game_over_text
-            break
+            # Game Over
+            if enemyY[i] > 440:
+                for j in range(num_of_enemies):
+                    enemyY[j] = 2000
+                game_over = True
+                break
 
-        enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 0.3
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 736:
-            enemyX_change[i] = -0.3
-            enemyY[i] += enemyY_change[i]
+            enemyX[i] += enemyX_change[i]
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 0.3
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 736:
+                enemyX_change[i] = -0.3
+                enemyY[i] += enemyY_change[i]
 
-        # Collision
-        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
-        if collision:
-            explosion_Sound = mixer.Sound('explosion.wav')
-            explosion_Sound.play()
-            bulletY = 480
-            bullet_state = "ready"
-            score_value += 1
-            enemyX[i] = random.randint(0, 736)
-            enemyY[i] = random.randint(50, 150)
+            # Collision
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+                explosion_Sound = mixer.Sound('explosion.wav')
+                explosion_Sound.play()
+                bulletY = 480
+                bullet_state = "ready"
+                score_value += 1
+                enemyX[i] = random.randint(0, 736)
+                enemyY[i] = random.randint(50, 150)
 
-        enemy(enemyX[i], enemyY[i], i)
+            enemy(enemyX[i], enemyY[i], i)
 
-    # Bullet Movement
-    if bullet_state == "fire":
-        screen.blit(bulletImg, (bulletX, bulletY))
-        bulletY -= bulletY_change
-        if bulletY <= 0:
-            bullet_state = "ready"
-        print(f"Bullet coordinates: ({bulletX}, {bulletY})")
+        # Bullet Movement
+        if bullet_state == "fire":
+            screen.blit(bulletImg, (bulletX, bulletY))
+            bulletY -= bulletY_change
+            if bulletY <= 0:
+                bullet_state = "ready"
+            print(f"Bullet coordinates: ({bulletX}, {bulletY})")
 
-    
+        player(playerX, playerY)
+        show_score(textX, textY)
+    else:
+        game_over_text()
 
-    player(playerX, playerY)
-    show_score(textX, textY)
     pygame.display.update()
